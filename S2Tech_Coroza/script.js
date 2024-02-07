5f8dbdba43d7f7373589d42ab50ae04a649e6c75
ajivc17 <157089721+ajivc17@users.noreply.github.com>
2024-02-07 23:29:45 +0800

    Add files via upload

---
+++ S2Tech_Coroza/script.js
@@ -0,0 +1,76 @@
+document.getElementById('monday').addEventListener('click', function() {
+    var box = document.getElementById('box');
+    box.style.display = 'block';
+    setTimeout(function() {
+      box.classList.add('move');
+      setTimeout(function() {
+        box.classList.add('change-color');
+      }, 1000);
+    }, 0);
+  });
+  
+  document.getElementById('tuesday').addEventListener('click', function() {
+    var box = document.getElementById('box');
+    box.style.display = 'block';
+    setTimeout(function() {
+      box.classList.add('move');
+      setTimeout(function() {
+        box.classList.add('change-color');
+      }, 1000);
+    }, 0);
+  });
+  
+  document.getElementById('wednesday').addEventListener('click', function() {
+    var box = document.getElementById('box');
+    box.style.display = 'block';
+    setTimeout(function() {
+      box.classList.add('move');
+      setTimeout(function() {
+        box.classList.add('change-color');
+      }, 1000);
+    }, 0);
+  });
+  
+  document.getElementById('thursday').addEventListener('click', function() {
+    var box = document.getElementById('box');
+    box.style.display = 'block';
+    setTimeout(function() {
+      box.classList.add('move');
+      setTimeout(function() {
+        box.classList.add('change-color');
+      }, 1000);
+    }, 0);
+  });
+  
+  document.getElementById('friday').addEventListener('click', function() {
+    var box = document.getElementById('box');
+    box.style.display = 'block';
+    setTimeout(function() {
+      box.classList.add('move');
+      setTimeout(function() {
+        box.classList.add('change-color');
+      }, 1000);
+    }, 0);
+  });
+  
+  document.getElementById('saturday').addEventListener('click', function() {
+    var box = document.getElementById('box');
+    box.style.display = 'block';
+    setTimeout(function() {
+      box.classList.add('move');
+      setTimeout(function() {
+        box.classList.add('change-color');
+      }, 1000);
+    }, 0);
+  });
+  
+  document.getElementById('sunday').addEventListener('click', function() {
+    var box = document.getElementById('box');
+    box.style.display = 'block';
+    setTimeout(function() {
+      box.classList.add('move');
+      setTimeout(function() {
+        box.classList.add('change-color');
+      }, 1000);
+    }, 0);
+  });
